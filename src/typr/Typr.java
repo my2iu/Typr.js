69ecf4160222c11c838376544620fecae9351756
Ming <github@programmingbasics.org>
2018-03-13 06:12:43 -0400

    Getting rid of direct references to JavaScript.

--- src/typr/Typr.java
+++ src/typr/Typr.java
@@ -7,6 +7,21 @@ import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 import typr.GwtTestEntryPoint.TyprFont;
+import typr.tabs.CFF;
+import typr.tabs.GPOS;
+import typr.tabs.GSUB;
+import typr.tabs.OS2;
+import typr.tabs.SVG;
+import typr.tabs.cmap;
+import typr.tabs.glyf;
+import typr.tabs.head;
+import typr.tabs.hhea;
+import typr.tabs.hmtx;
+import typr.tabs.kern;
+import typr.tabs.loca;
+import typr.tabs.maxp;
+import typr.tabs.name;
+import typr.tabs.post;
 
 @JsType(namespace=JsPackage.GLOBAL)
 public class Typr
@@ -78,12 +93,68 @@ public class Typr
 		var t = tags[i];
 		//console.log(t);
 		//if(tabs[t]) console.log(t, tabs[t].offset, tabs[t].length);
-		if(tabs[t]) obj[t.trim()] = Typr[t.trim()].parse(data, tabs[t].offset, tabs[t].length, obj);
+		if (tabs[t]) {
+		  var parsed = @typr.Typr::parseTab(Lcom/google/gwt/core/client/JavaScriptObject;Lcom/google/gwt/core/client/JavaScriptObject;IILjava/lang/String;)(obj,data,tabs[t].offset, tabs[t].length,t);
+		  if (parsed)
+		    obj[t.trim()] = parsed;
+		}
+//		if(tabs[t]) obj[t.trim()] = Typr[t.trim()].parse(data, tabs[t].offset, tabs[t].length, obj);
 	}
 	
 	return obj;
 }-*/;
 
+  static Object parseTab(JavaScriptObject obj, JavaScriptObject data, int offset, int length, String tag)
+  {
+    switch(tag)
+    {
+    case "cmap":
+      return cmap.parse(data, offset, length);
+    case "head":
+      return head.parse(data, offset, length);
+    case "hhea":
+      return hhea.parse(data, offset, length);
+    case "maxp":
+      return maxp.parse(data, offset, length);
+    case "hmtx":
+      return hmtx.parse(data, offset, length, obj);
+    case "name":
+      return name.parse(data, offset, length);
+    case "OS/2":
+      return OS2.parse(data, offset, length);
+    case "post":
+      return post.parse(data, offset, length);
+    
+    //"cvt",
+    //"fpgm",
+    case "loca":
+      return loca.parse(data, offset, length, obj);
+    case "glyf":
+      return glyf.parse(data, offset, length, obj);
+    case "kern":
+      return kern.parse(data, offset, length, obj);
+    
+    //"prep"
+    //"gasp"
+    
+    case "CFF ":
+      return CFF.parse(data, offset, length);
+    
+    
+    case "GPOS":
+      return GPOS.parse(data, offset, length, obj);
+    case "GSUB":
+      return GSUB.parse(data, offset, length, obj);
+    
+    case "SVG ":
+      return SVG.parse(data, offset, length);
+    //"VORG",
+    
+    }
+    return null;
+    
+  }
+  
   @JsMethod public static native JavaScriptObject _tabOffset (JavaScriptObject data, JavaScriptObject tab)
 /*-{
 	var bin = Typr._bin;
